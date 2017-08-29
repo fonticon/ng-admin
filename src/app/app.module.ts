9b62c3fef4bdddcb3153eae021529a4a4c7eb7cc
Dharmesh <pipariyadharmesh@gmail.com>
2017-08-29 23:34:42 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { appRoutes } from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
